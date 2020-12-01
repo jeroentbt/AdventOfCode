04a6d2fba7437ad4a151683946168980f796e92c
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-01 21:31:56 +0100

    green

--- day1/test_expense_report.py
+++ day1/test_expense_report.py
@@ -8,10 +8,18 @@ def test_find_entries_that_add_up_t0_2020():
     list = [1721, 979, 366, 299, 675, 1456]
     assert [1721, 299] == find_entries_that_add_up_to_2020(list)
 
+def test_find_entries_that_add_up_t0_2020_2():
+    list = [1722, 979, 366, 298, 675, 1456]
+    assert [1722, 298] == find_entries_that_add_up_to_2020(list)
+
     
 def get_rest(given_number):
     return 2020 - given_number
 
-
+def find_entries_that_add_up_to_2020(list):
+    for number in list:
+        other_number = get_rest(number)
+        if other_number in list:
+            return [number, other_number]
 
     
