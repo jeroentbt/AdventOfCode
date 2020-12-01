ee9b8f935075864dc60b24ece40a8eb9a9800af4
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-01 20:10:06 +0100

    red

--- day1/test_expense_report.py
+++ day1/test_expense_report.py
@@ -4,6 +4,14 @@ def test_get_rest_of_2019():
 def test_get_rest_of_1721():
     assert get_rest(1721) == 299
 
+def test_find_entries_that_add_up_t0_2020():
+    list = [1721, 979, 366, 299, 675, 1456]
+    assert [1721, 299] == find_entries_that_add_up_to_2020(list)
+
     
 def get_rest(given_number):
     return 2020 - given_number
+
+
+
+    
