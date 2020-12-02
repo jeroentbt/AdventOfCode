e0f7b0c0f685be5e4ebd99205a48d3ccaad1e465
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-03 00:38:16 +0100

    chack policy

--- day2/passwords.py
+++ day2/passwords.py
@@ -1,2 +1,14 @@
 def get_policy(policystring):
-    return tuple([int(i) for i in policystring.split('-')])
+    policy = policystring.split(' ')
+    policylist = [int(i) for i in policy[0].split('-')]
+    policylist.append(policy[1])
+    return tuple(policylist)
+
+
+def check_policy(listitem):
+    policystring, password = tuple(listitem.split(': '))
+    min_occurence, max_occurence, occurence_char = get_policy(policystring)
+    if min_occurence <= password.count(occurence_char) <= max_occurence:
+        return True
+    else:
+        return False
--- day2/test_passwords.py
+++ day2/test_passwords.py
@@ -2,8 +2,15 @@ from passwords import *
 
 
 def test_get_policy():
-    assert get_policy("1-3") == (1, 3)
+    assert get_policy("1-3 a") == (1, 3, 'a')
 
 
-def test_get_policy():
-    assert get_policy("10-13") == (10, 13)
+def test_get_policy_2():
+    assert get_policy("10-13 b") == (10, 13, 'b')
+
+
+def test_check_policy():
+    assert check_policy("1-2 a: aa") == True
+
+def test_check_policy_should_fail():
+    assert check_policy("1-2 a: aaa") == False
