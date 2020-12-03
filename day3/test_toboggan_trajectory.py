ac95fe24384972c34b548016aa0b50139275b5ae
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-03 19:48:09 +0100

    finding trees, even in repeated landscape

--- day3/test_toboggan_trajectory.py
+++ day3/test_toboggan_trajectory.py
@@ -1,7 +1,19 @@
-from toboggan_trajectory import move
+from toboggan_trajectory import thing_at_position
 
 
-def test_report_tree_after_single_horizontal_move():
-    slope = (3, 0)
+def test_report_tree_at_position():
     landscape = "...#"
-    assert "tree" == move(landscape, slope)
+    position = (0, 3)
+    assert "tree" == thing_at_position(landscape, position)
+
+
+def test_report_snow_at_position():
+    landscape = "...#"
+    position = (0, 2)
+    assert "snow" == thing_at_position(landscape, position)
+
+
+def test_report_snow_at_position_out_of_bounds():
+    landscape = "...#"
+    position = (0, 4)
+    assert "snow" == thing_at_position(landscape, position)
--- day3/toboggan_trajectory.py
+++ day3/toboggan_trajectory.py
@@ -1,2 +1,8 @@
-def move(landscape, slope):
-    return "tree"
+def thing_at_position(landscape, position):
+    landscape_list = landscape.splitlines()
+    row, column = position
+    column = column % len(landscape_list[0])
+    if landscape_list[row][column] == "#":
+        return "tree"
+    else:
+        return "snow"
