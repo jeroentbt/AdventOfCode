aacf060e0236f47509eab5d0e3c51654fc08b428
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-03 21:14:53 +0100

    day3 done

--- day3/test_toboggan_trajectory.py
+++ day3/test_toboggan_trajectory.py
@@ -1,4 +1,4 @@
-from toboggan_trajectory import thing_at_position, slide
+from toboggan_trajectory import thing_at_position, slide, count_trees, multiply_options
 
 
 def test_report_tree_at_position():
@@ -40,9 +40,52 @@ def test_moving_vertically_2():
     assert ["snow"] == slide(landscape, slope)
 
 
+def test_moving_too_far_vertically():
+    landscape = ("####\n"
+                 ".###")
+    slope = (0, 2)
+    assert ["done"] == slide(landscape, slope)
+
+
 def test_moving_on_a_slope():
     landscape = ("...#.\n"
                  "..#..\n"
                  "#....")
     slope = (2, 1)
     assert ["tree", "snow"] == slide(landscape, slope)
+
+
+def test_count_trees():
+    landscape = ("..##.......\n"
+                 "#...#...#..\n"
+                 ".#....#..#.\n"
+                 "..#.#...#.#\n"
+                 ".#...##..#.\n"
+                 "..#.##.....\n"
+                 ".#.#.#....#\n"
+                 ".#........#\n"
+                 "#.##...#...\n"
+                 "#...##....#\n"
+                 ".#..#...#.#")
+    slope = (3, 1)
+    assert 7 == count_trees(landscape, slope)
+
+
+def test_multiply_options():
+    landscape = ("..##.......\n"
+                 "#...#...#..\n"
+                 ".#....#..#.\n"
+                 "..#.#...#.#\n"
+                 ".#...##..#.\n"
+                 "..#.##.....\n"
+                 ".#.#.#....#\n"
+                 ".#........#\n"
+                 "#.##...#...\n"
+                 "#...##....#\n"
+                 ".#..#...#.#")
+    slopes = [(1, 1),
+              (3, 1),
+              (5, 1),
+              (7, 1),
+              (1, 2)]
+    assert 336 == multiply_options(landscape, slopes)
--- day3/toboggan_trajectory.py
+++ day3/toboggan_trajectory.py
@@ -2,7 +2,9 @@ def thing_at_position(landscape, position):
     landscape_list = landscape.splitlines()
     row, column = position
     column = column % len(landscape_list[0])
-    if landscape_list[row][column] == "#":
+    if row >= len(landscape_list):
+        return "done"
+    elif landscape_list[row][column] == "#":
         return "tree"
     else:
         return "snow"
@@ -12,10 +14,33 @@ def slide(landscape, slope):
     horizontal_movement, vertical_movement = slope
     encountered_landscape = []
     for step in range(1, len(landscape.splitlines())):
-        print("step")
-        position = (0 + (step * vertical_movement), 0 + (step *horizontal_movement))
-        print(landscape)
-        print(position)
-        print(thing_at_position(landscape, position))
+        position = (0 + (step * vertical_movement), 0 + (step * horizontal_movement))
         encountered_landscape.append(thing_at_position(landscape, position))
     return encountered_landscape
+
+
+def count_trees(landscape, slope):
+    return slide(landscape, slope).count("tree")
+
+
+def multiply_options(landscape, slopes):
+    result = 1
+    for slope in slopes:
+        print(count_trees(landscape, slope))
+        result *= count_trees(landscape, slope)
+    return result
+
+
+if __name__ == "__main__":
+    with open("input.txt") as landscape_file:
+        landscape = landscape_file.read()
+        slope = (3, 1)
+        # should be 292
+        print(count_trees(landscape, slope))
+        slopes = [(1, 1),
+                  (3, 1),
+                  (5, 1),
+                  (7, 1),
+                  (1, 2)]
+        # should be 9354744432
+        print(multiply_options(landscape, slopes))
