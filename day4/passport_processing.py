abb5e74f0f9c42da2eb4d21453deb6ffe67917bb
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-04 22:47:36 +0100

    refactor

--- day4/passport_processing.py
+++ day4/passport_processing.py
@@ -1,4 +1,8 @@
 class Passport:
 
     def __init__(self, passport_string):
-        self.birthyear = int(passport_string.split(':')[1])
+        self.birthyear = int(self.split_kv(passport_string)[1])
+
+
+    def split_kv(self, kv_pair):
+        return tuple(kv_pair.split(':'))
