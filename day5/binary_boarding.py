2e0f0294d51100aa160988d79e0286644220e38e
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-05 16:18:33 +0100

    column number

--- day5/binary_boarding.py
+++ day5/binary_boarding.py
@@ -1,3 +1,8 @@
 def return_row(rowstring):
     binary_rowstring = rowstring.replace('B', '1').replace('F', '0')
     return int(binary_rowstring, 2)
+
+
+def return_column(columnstring):
+    binary_columnstring = columnstring.replace('R', '1').replace('L', '0')
+    return int(binary_columnstring, 2)
--- day5/test_binary_boarding.py
+++ day5/test_binary_boarding.py
@@ -1,4 +1,4 @@
-from binary_boarding import return_row
+from binary_boarding import return_row, return_column
 
 def test_row_number_from_string():
     input = "BFFFBBF"
@@ -8,3 +8,13 @@ def test_row_number_from_string():
 def test_row_number_from_string_2():
     input = 'FFFBBBF'
     assert 14 == return_row(input)
+
+
+def test_column_number_from_string():
+    input = 'RRR'
+    assert 7 == return_column(input)
+
+
+def test_column_number_from_string_2():
+    input = 'RLL'
+    assert 4 == return_column(input)
