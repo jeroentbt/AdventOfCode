4965845ed14af852c422f9bbb816bab7835b4297
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-05 17:54:46 +0100

    add test

--- day5/test_binary_boarding.py
+++ day5/test_binary_boarding.py
@@ -1,4 +1,4 @@
-from binary_boarding import Seat
+from binary_boarding import Seat, highest_seat_id
 import pytest
 
 @pytest.mark.parametrize("boardingpass, row, column, ID",
@@ -12,3 +12,8 @@ def test_row_seat(boardingpass, row, column, ID):
     assert row == seat.row
     assert column == seat.column
     assert ID == seat.ID
+
+
+def test_highest_seat_id():
+    boardingpasses = ['BFFFBBFRRR', 'BBFFBBFRLL', 'FFFBBBFRRR']
+    assert 820 == highest_seat_id(boardingpasses)
