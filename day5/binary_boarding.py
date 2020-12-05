f42fe864f8cc685d0f5c382744de4b37c395e19a
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-05 17:52:29 +0100

    find highest seat.id (no test)

--- day5/binary_boarding.py
+++ day5/binary_boarding.py
@@ -11,3 +11,17 @@ class Seat():
                               .replace('R', '1') \
                               .replace('L', '0')
         return int(binary_string, 2)
+
+
+def highest_seat_id(boardingpasses):
+    highest = 0
+    for boardingpass in boardingpasses:
+        seat = Seat(boardingpass)
+        if seat.ID > highest:
+            highest = seat.ID
+    return highest
+
+if __name__ == "__main__":
+    with open("input.txt") as boardingpasses_list:
+        boardingpasses = boardingpasses_list.read().splitlines()
+        print(highest_seat_id(boardingpasses))
