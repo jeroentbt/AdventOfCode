89c8217635aa395dd7f7c244f96cf155c6d424ed
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-05 16:53:39 +0100

    refactor, one test left...

--- day5/binary_boarding.py
+++ day5/binary_boarding.py
@@ -1,12 +1,13 @@
-def number_from_seatstring(string):
-    binary_string = string.replace('B', '1') \
-                          .replace('F', '0') \
-                          .replace('R', '1') \
-                          .replace('L', '0')
-    return int(binary_string, 2)
-
 class Seat():
     def __init__(self, seatstring):
-        self.row = number_from_seatstring(seatstring[:7])
-        self.column = number_from_seatstring(seatstring[7:])
+        self.row = self.to_number(seatstring[:7])
+        self.column = self.to_number(seatstring[7:])
         self.ID = (self.row * 8) + self.column
+
+
+    def to_number(self, string):
+        binary_string = string.replace('B', '1') \
+                              .replace('F', '0') \
+                              .replace('R', '1') \
+                              .replace('L', '0')
+        return int(binary_string, 2)
--- day5/test_binary_boarding.py
+++ day5/test_binary_boarding.py
@@ -1,24 +1,4 @@
-from binary_boarding import number_from_seatstring, Seat
-
-def test_row_number_from_string():
-    input = "BFFFBBF"
-    assert 70 == number_from_seatstring(input)
-
-
-def test_row_number_from_string_2():
-    input = 'FFFBBBF'
-    assert 14 == number_from_seatstring(input)
-
-
-def test_column_number_from_string():
-    input = 'RRR'
-    assert 7 == number_from_seatstring(input)
-
-
-def test_column_number_from_string_2():
-    input = 'RLL'
-    assert 4 == number_from_seatstring(input)
-
+from binary_boarding import Seat
 
 def test_row_seat():
     input = "BFFFBBFRRR"
