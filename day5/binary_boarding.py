9e085c6be97cc3d73d12f4ef015cb208ea655c22
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-05 18:07:48 +0100

    refactor

--- day5/binary_boarding.py
+++ day5/binary_boarding.py
@@ -13,19 +13,16 @@ class Seat():
         return int(binary_string, 2)
 
 
-def list_seat_ids(boardingpasses):
+def list_seat_ids_sorted(boardingpasses):
     list = []
     for boardingpass in boardingpasses:
         seat = Seat(boardingpass)
         list.append(seat.ID)
+    list.sort()
     return list
 
 def highest_seat_id(boardingpasses):
-    highest = 0
-    for ID in list_seat_ids(boardingpasses):
-        if ID > highest:
-            highest = ID
-    return highest
+    return list_seat_ids_sorted(boardingpasses)[-1]
 
 if __name__ == "__main__":
     with open("input.txt") as boardingpasses_list:
