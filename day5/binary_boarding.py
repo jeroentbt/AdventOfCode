1844012f83a708393e06ab8079e581b2e01f2582
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-05 16:15:17 +0100

    rowstring to number

--- day5/binary_boarding.py
+++ day5/binary_boarding.py
@@ -1,2 +1,3 @@
 def return_row(rowstring):
-    return 70
+    binary_rowstring = rowstring.replace('B', '1').replace('F', '0')
+    return int(binary_rowstring, 2)
--- day5/test_binary_boarding.py
+++ day5/test_binary_boarding.py
@@ -3,3 +3,8 @@ from binary_boarding import return_row
 def test_row_number_from_string():
     input = "BFFFBBF"
     assert 70 == return_row(input)
+
+
+def test_row_number_from_string_2():
+    input = 'FFFBBBF'
+    assert 14 == return_row(input)
