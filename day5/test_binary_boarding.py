43292641be333f6ad5818f3410ee356356de07b0
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-05 16:25:36 +0100

    refactor

--- day5/test_binary_boarding.py
+++ day5/test_binary_boarding.py
@@ -1,20 +1,20 @@
-from binary_boarding import return_row, return_column
+from binary_boarding import return_row, return_column, number_from_seatstring
 
 def test_row_number_from_string():
     input = "BFFFBBF"
-    assert 70 == return_row(input)
+    assert 70 == number_from_seatstring(input)
 
 
 def test_row_number_from_string_2():
     input = 'FFFBBBF'
-    assert 14 == return_row(input)
+    assert 14 == number_from_seatstring(input)
 
 
 def test_column_number_from_string():
     input = 'RRR'
-    assert 7 == return_column(input)
+    assert 7 == number_from_seatstring(input)
 
 
 def test_column_number_from_string_2():
     input = 'RLL'
-    assert 4 == return_column(input)
+    assert 4 == number_from_seatstring(input)
