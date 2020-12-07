e50349109969c5ca34918bcfe3757d7825d3cc99
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-07 21:16:05 +0100

    refactor

--- day7/handy_haversacks.py
+++ day7/handy_haversacks.py
@@ -3,7 +3,8 @@ import re
 
 def read_rule(rule):
     r = re.match(r"^(\w+ \w+) bags contain (.*)", rule)
-    if r.group(1) == "light red":
-        return {"light red": [("bright red", 1), ("dark red"), 1]}
+    container = r.group(1)
+    if container == "light red":
+        return {container: [("bright red", 1), ("dark red"), 1]}
     else:
-        return {"light green": []}
+        return {container: []}
