3b5d5ff691c5fb3bc4b462b6b3c08d1c0a237cbd
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-07 21:17:35 +0100

    refactor

--- day7/handy_haversacks.py
+++ day7/handy_haversacks.py
@@ -4,7 +4,8 @@ import re
 def read_rule(rule):
     r = re.match(r"^(\w+ \w+) bags contain (.*)", rule)
     container = r.group(1)
-    if container == "light red":
-        return {container: [("bright red", 1), ("dark red"), 1]}
-    else:
+    contents = r.group(2)
+    if contents == "no other bags":
         return {container: []}
+    else:
+        return {container: [("bright red", 1), ("dark red"), 1]}
