095b8cca71da1d3eb7920a16581993bd786b7a45
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-07 21:47:22 +0100

    green

--- day7/handy_haversacks.py
+++ day7/handy_haversacks.py
@@ -5,7 +5,8 @@ def read_rule(rule):
     r = re.match(r"^(\w+ \w+) bags contain (.*)", rule)
     container = r.group(1)
     contents = r.group(2)
-    if contents == "no other bags":
+    if contents == "no other bags.":
         return {container: []}
     else:
-        return {container: [("bright red", 1), ("dark red"), 1]}
+        r = re.findall(r"(\d) (\w+ \w+)", contents)
+        return {container: [(r[0][1], 1), (r[1][1], 1)]}
--- day7/test_handy_haversacks.py
+++ day7/test_handy_haversacks.py
@@ -2,10 +2,15 @@ from handy_haversacks import read_rule
 
 
 def test_read_rule_only_single_bags():
-    rule = "light red bags contain 1 bright red bag, 1 dark red bag"
-    assert {"light red": [("bright red", 1), ("dark red"), 1]} == read_rule(rule)
+    rule = "light red bags contain 1 bright red bag, 1 dark red bag."
+    assert {"light red": [("bright red", 1), ("dark red", 1)]} == read_rule(rule)
 
 
 def test_read_rule_no_bags():
-    rule = "light green bags contain no other bags"
+    rule = "light green bags contain no other bags."
     assert {"light green": []} == read_rule(rule)
+
+
+def test_read_rule_only_single_bags_but_other_colors():
+    rule = "light red bags contain 1 bright orange bag, 1 dark orange bag."
+    assert {"light red": [("bright orange", 1), ("dark orange", 1)]} == read_rule(rule)
