20b2ca635b4cb1466741e63f459962452675e964
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-09 00:48:10 +0100

    brute-forced part2...

--- day8/handheld_halting.py
+++ day8/handheld_halting.py
@@ -15,10 +15,28 @@ def run_program(program, result=0, next_step=0):
         next_step += argument if operation == 'jmp' else 1
         if next_step < len(program):
             result = run_program(program, result, next_step)
+        else:
+            print('program terminated succesfully')
     return result
+
+
+def fix_program(program):
+    for i, instruction in enumerate(program):
+        modified_program = program.copy()
+        operation, argument, runs = instruction
+        if operation == 'nop' or operation == 'jmp':
+            switcheroo = {'nop': 'jmp',
+                          'jmp': 'nop'}
+            operation = switcheroo[operation]
+            modified_program[i] = (operation, argument, runs)
+            print(run_program(modified_program))
 
 
 if __name__ == "__main__":
     with open("input.txt") as progfile:
-        print('one run results in:')
-        print(run_program(read_prog(progfile.read())))
+        program = read_prog(progfile.read())
+        second_run = program.copy()
+    print('one run results in:')
+    print(run_program(program))
+    print('---')
+    fix_program(second_run)
