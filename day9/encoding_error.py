45eba5d0449b521184add2d1c42c545cfb0def89
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-10 00:19:18 +0100

    cleanup

--- day9/encoding_error.py
+++ day9/encoding_error.py
@@ -1,7 +1,6 @@
 def is_sum_of_2_in(the_sum, the_numbers):
     for n in the_numbers:
         rest = the_sum - n
-        print(rest, the_numbers.count(rest))
         if rest in the_numbers and (rest != n or the_numbers.count(n) > 1):
             return True
     return False
