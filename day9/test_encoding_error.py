411581ef9b3bed9c0feda93173dca259bab4b453
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-10 00:18:26 +0100

    green

--- day9/test_encoding_error.py
+++ day9/test_encoding_error.py
@@ -23,3 +23,9 @@ def test_is_not_sum_of_2_in_3():
     the_sum = 10
     the_numbers = [5, 8, 1]
     assert False == is_sum_of_2_in(the_sum, the_numbers)
+
+
+def test_is_sum_of_2_of_the_same_in_3():
+    the_sum = 10
+    the_numbers = [5, 8, 5]
+    assert True == is_sum_of_2_in(the_sum, the_numbers)
