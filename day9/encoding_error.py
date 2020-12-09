6576a9b8bd3d8204ef6c5da6eff351bf090d8730
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-10 00:58:07 +0100

    day 9 part 1 done

--- day9/encoding_error.py
+++ day9/encoding_error.py
@@ -4,3 +4,17 @@ def is_sum_of_2_in(the_sum, the_numbers):
         if rest in the_numbers and (rest != n or the_numbers.count(n) > 1):
             return True
     return False
+
+
+def first_number_that_is_not_sum_of_two_in_preamble(preamble, the_list):
+    for i, n in enumerate(the_list[preamble:]):
+        start_of_preamble = i
+        end_of_preamble = i + preamble
+        if not is_sum_of_2_in(n, the_list[start_of_preamble:end_of_preamble]):
+            return n
+
+
+if __name__ == "__main__":
+    with open("input.txt") as XMASdata:
+        XMAS = [int(i) for i in XMASdata.readlines()]
+        print(first_number_that_is_not_sum_of_two_in_preamble(25, XMAS))
--- day9/test_encoding_error.py
+++ day9/test_encoding_error.py
@@ -1,4 +1,5 @@
-from encoding_error import is_sum_of_2_in
+from encoding_error import is_sum_of_2_in, \
+    first_number_that_is_not_sum_of_two_in_preamble
 
 
 def test_is_sum_of_2():
@@ -29,3 +30,24 @@ def test_is_sum_of_2_of_the_same_in_3():
     the_sum = 10
     the_numbers = [5, 8, 5]
     assert True == is_sum_of_2_in(the_sum, the_numbers)
+
+
+def test_find_first_number_that_fails_test_1():
+    the_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+                14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 100]
+    preamble = 25
+    assert 100 == first_number_that_is_not_sum_of_two_in_preamble(preamble, the_list)
+
+
+def test_find_first_number_that_fails_test_2():
+    the_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+                14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 3, 100]
+    preamble = 25
+    assert 100 == first_number_that_is_not_sum_of_two_in_preamble(preamble, the_list)
+
+
+def test_find_first_number_that_fails_test_3():
+    the_list = [35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102,
+                117, 150, 182, 127, 219, 299, 277, 309, 576]
+    preamble = 5
+    assert 127 == first_number_that_is_not_sum_of_two_in_preamble(preamble, the_list)
