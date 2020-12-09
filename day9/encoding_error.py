be942bce485b5667ffa2df4c5b7b3f44e813d0fb
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-10 00:15:50 +0100

    green

--- day9/encoding_error.py
+++ day9/encoding_error.py
@@ -1,6 +1,7 @@
 def is_sum_of_2_in(the_sum, the_numbers):
     for n in the_numbers:
         rest = the_sum - n
-        if rest in the_numbers:
+        print(rest, the_numbers.count(rest))
+        if rest in the_numbers and rest != n:
             return True
     return False
--- day9/test_encoding_error.py
+++ day9/test_encoding_error.py
@@ -17,3 +17,9 @@ def test_is_sum_of_2_in_3():
     the_sum = 10
     the_numbers = [2, 8, 1]
     assert True == is_sum_of_2_in(the_sum, the_numbers)
+
+
+def test_is_not_sum_of_2_in_3():
+    the_sum = 10
+    the_numbers = [5, 8, 1]
+    assert False == is_sum_of_2_in(the_sum, the_numbers)
