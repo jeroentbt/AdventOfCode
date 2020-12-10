de9b27e592537ffcd3cf9c359ddb6a8580d179fc
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-10 20:49:24 +0100

    add final adapter and count diffs

--- day10/adapter_array.py
+++ day10/adapter_array.py
@@ -4,4 +4,13 @@ def list_jumps(adapters):
     for i, joltage in enumerate(adapters[:-1]):
         next_joltage = adapters[i+1]
         jumps.append(next_joltage - joltage)
+    jumps.append(3)  # internal adapter is 3 jolts higher than highest output
     return jumps
+
+
+def diffs(jumps):
+    diffs = {}
+    diffs['one'] = jumps.count(1)
+    diffs['two'] = jumps.count(2)
+    diffs['three'] = jumps.count(3)
+    return diffs
--- day10/test_adapter_array.py
+++ day10/test_adapter_array.py
@@ -1,16 +1,21 @@
-from adapter_array import list_jumps
+from adapter_array import list_jumps, diffs
 
 
 def test_no_jumps():
     input = [1, 2, 3, 4, 5]
-    assert [1, 1, 1, 1] == list_jumps(input)
+    assert [1, 1, 1, 1, 3] == list_jumps(input)
 
 
 def test_1_jump_of_2():
     input = [1, 2, 3, 4, 6]
-    assert [1, 1, 1, 2] == list_jumps(input)
+    assert [1, 1, 1, 2, 3] == list_jumps(input)
 
 
 def test_1_jump_of_2_unordered():
     input = [1, 6, 3, 4, 2]
-    assert [1, 1, 1, 2] == list_jumps(input)
+    assert [1, 1, 1, 2, 3] == list_jumps(input)
+
+
+def test_return_number_of_differences():
+    input = [1, 1, 2, 1, 3]
+    assert {'one': 3, 'two': 1, 'three': 1} == diffs(input)
