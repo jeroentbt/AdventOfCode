bafafc26946075886404952aa1f776239191b17f
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-12 23:51:16 +0100

    moving north

--- day12/rain_risk.py
+++ day12/rain_risk.py
@@ -1,3 +1,14 @@
 class Boat():
     def __init__(self):
-        self.facing = "east"
+        self.facing = "E"
+        self.latitude = 0  # N-S
+        self.longtitude = 0  # E-W
+
+    def move(self, instruction):
+        action, value = instruction
+        if action == "N":
+            self.latitude += value
+
+
+def read_nav(nav_txt):
+    return [(str(l[:1]), int(l[1:])) for l in nav_txt.splitlines()]
--- day12/test_rain_risk.py
+++ day12/test_rain_risk.py
@@ -1,6 +1,20 @@
-from rain_risk import Boat
+from rain_risk import Boat, read_nav
 
 
-def test_boat_starts_facing_east():
+def test_read_nav():
+    input = ("F10\n"
+             "N3\n"
+             "F7\n"
+             "R90\n"
+             "F11")
+    assert [("F", 10),
+            ("N", 3),
+            ("F", 7),
+            ("R", 90),
+            ("F", 11)] == read_nav(input)
+
+
+def test_move_north():
     b = Boat()
-    assert "east" == b.facing
+    b.move(("N", 10))
+    assert 10 == b.latitude
