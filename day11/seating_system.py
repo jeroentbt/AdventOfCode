f54b1ceddb1fc489c20f49a2ffc83060829ade25
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-12 15:05:49 +0100

    reading grid

--- day11/seating_system.py
+++ day11/seating_system.py
@@ -1,4 +1,8 @@
 def read_grid(input_txt):
-    return [[".", ".", "."],
-            [".", ".", "."],
-            [".", ".", "."]]
+    rows = input_txt.splitlines()
+    grid = [[position for position in row] for row in rows]
+    return grid
+
+
+def adjacent_occupied_seats_for(row, column, grid):
+    return 0
--- day11/test_seating_system.py
+++ day11/test_seating_system.py
@@ -1,9 +1,26 @@
-from seating_system import read_grid
+from seating_system import read_grid, adjacent_occupied_seats_for
 
 
 def test_read_grid_all_floor():
-    input = ("..."
-             "..."
+    input = ("...\n"
+             "...\n"
              "...")
     grid = read_grid(input)
-    assert "." == grid[1][1]
+    assert "." == grid[0][0]
+
+
+def test_read_grid_one_seat():
+    input = ("...\n"
+             ".L.\n"
+             "...")
+    grid = read_grid(input)
+    assert "." == grid[0][0]
+    assert "L" == grid[1][1]
+
+
+def test_number_of_adjacent_occupied_seats():
+    input = ("...\n"
+             ".L.\n"
+             "...")
+    grid = read_grid(input)
+    assert 0 == adjacent_occupied_seats_for(1, 1, grid)
