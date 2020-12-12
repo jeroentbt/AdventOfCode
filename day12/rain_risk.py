0ef9ef2cf3cfd141d593f7d66afb8f00e2e7d49f
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-13 00:03:18 +0100

    turning

--- day12/rain_risk.py
+++ day12/rain_risk.py
@@ -1,6 +1,6 @@
 class Boat():
     def __init__(self):
-        self.facing = "E"
+        self.facing = 90
         self.latitude = 0  # N-S
         self.longtitude = 0  # E-W
 
@@ -8,7 +8,20 @@ class Boat():
         action, value = instruction
         if action == "N":
             self.latitude += value
+        if action == "S":
+            self.latitude -= value
+        if action == "E":
+            self.longtitude += value
+        if action == "W":
+            self.longtitude -= value
+        if action == "R":
+            self.facing += value
+        if action == "L":
+            self.facing -= value
+
+
+
 
 
 def read_nav(nav_txt):
-    return [(str(l[:1]), int(l[1:])) for l in nav_txt.splitlines()]
+    return [(str(line[:1]), int(line[1:])) for line in nav_txt.splitlines()]
--- day12/test_rain_risk.py
+++ day12/test_rain_risk.py
@@ -18,3 +18,35 @@ def test_move_north():
     b = Boat()
     b.move(("N", 10))
     assert 10 == b.latitude
+
+
+def test_move_south():
+    b = Boat()
+    b.latitude = 10
+    b.move(("S", 5))
+    assert 5 == b.latitude
+
+
+def test_move_east():
+    b = Boat()
+    b.move(("E", 10))
+    assert 10 == b.longtitude
+
+
+def test_move_west():
+    b = Boat()
+    b.longtitude = 10
+    b.move(("W", 5))
+    assert 5 == b.longtitude
+
+
+def test_turn_right():
+    b = Boat()
+    b.move(("R", 90))
+    assert 180 == b.facing
+
+
+def test_turn_left():
+    b = Boat()
+    b.move(("L", 100))
+    assert -10 == b.facing
