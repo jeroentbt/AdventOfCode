83cbd9dc70a4f7cfc35a99f4a6f85dbe247b749f
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-12 15:52:40 +0100

    first evolution

--- day11/seating_system.py
+++ day11/seating_system.py
@@ -5,4 +5,50 @@ def read_grid(input_txt):
 
 
 def adjacent_occupied_seats_for(row, column, grid):
-    return 0
+    occupied = "#"
+    number_of_occupied_seats = 0
+
+    nmb_of_rows = len(grid) - 1
+    nmb_of_cols = len(grid[0]) - 1
+
+    if row > 0 and column > 0 and \
+       grid[row-1][column-1] == occupied:
+        number_of_occupied_seats += 1
+    if row > 0 and \
+       grid[row-1][column] == occupied:
+        number_of_occupied_seats += 1
+    if row > 0 and column < nmb_of_cols and \
+       grid[row-1][column+1] == occupied:
+        number_of_occupied_seats += 1
+
+    if column > 0 and \
+       grid[row][column-1] == occupied:
+        number_of_occupied_seats += 1
+    if column < nmb_of_cols and \
+       grid[row][column+1] == occupied:
+        number_of_occupied_seats += 1
+
+    if row < nmb_of_rows and column > 0 and \
+       grid[row+1][column-1] == occupied:
+        number_of_occupied_seats += 1
+    if row < nmb_of_rows and \
+       grid[row+1][column] == occupied:
+        number_of_occupied_seats += 1
+    if row < nmb_of_rows and column < nmb_of_cols and \
+       grid[row+1][column+1] == occupied:
+        number_of_occupied_seats += 1
+    return number_of_occupied_seats
+
+
+def evolve(grid):
+    evolved_grid = []
+    for row_num, row in enumerate(grid):
+        evolved_grid.append([])
+        for col_num, position in enumerate(row):
+            evolved_grid[row_num].append("")
+            if position == "L" and \
+               adjacent_occupied_seats_for(row_num, col_num, grid) == 0:
+                evolved_grid[row_num][col_num] = "#"
+            else:
+                evolved_grid[row_num][col_num] = grid[row_num][col_num]
+    return evolved_grid
--- day11/test_seating_system.py
+++ day11/test_seating_system.py
@@ -1,4 +1,4 @@
-from seating_system import read_grid, adjacent_occupied_seats_for
+from seating_system import read_grid, adjacent_occupied_seats_for, evolve
 
 
 def test_read_grid_all_floor():
@@ -18,9 +18,76 @@ def test_read_grid_one_seat():
     assert "L" == grid[1][1]
 
 
-def test_number_of_adjacent_occupied_seats():
+def test_number_of_adjacent_occupied_seats_on_empty_floor():
     input = ("...\n"
              ".L.\n"
              "...")
     grid = read_grid(input)
     assert 0 == adjacent_occupied_seats_for(1, 1, grid)
+
+
+def test_one_seat_taken():
+    input = (".#.\n"
+             ".L.\n"
+             "...")
+    grid = read_grid(input)
+    assert 1 == adjacent_occupied_seats_for(1, 1, grid)
+
+
+def test_corners():
+    input = (".#.\n"
+             ".L.\n"
+             "...")
+    grid = read_grid(input)
+    assert 1 == adjacent_occupied_seats_for(0, 0, grid)
+    assert 1 == adjacent_occupied_seats_for(0, 2, grid)
+    assert 0 == adjacent_occupied_seats_for(2, 0, grid)
+    assert 0 == adjacent_occupied_seats_for(2, 2, grid)
+
+
+def test_empty_seats_with_no_neighbours_get_occupied():
+    grid_in = read_grid("L.LL.LL.LL\n"
+                        "LLLLLLL.LL\n"
+                        "L.L.L..L..\n"
+                        "LLLL.LL.LL\n"
+                        "L.LL.LL.LL\n"
+                        "L.LLLLL.LL\n"
+                        "..L.L.....\n"
+                        "LLLLLLLLLL\n"
+                        "L.LLLLLL.L\n"
+                        "L.LLLLL.LL")
+    grid_out = read_grid("#.##.##.##\n"
+                         "#######.##\n"
+                         "#.#.#..#..\n"
+                         "####.##.##\n"
+                         "#.##.##.##\n"
+                         "#.#####.##\n"
+                         "..#.#.....\n"
+                         "##########\n"
+                         "#.######.#\n"
+                         "#.#####.##")
+    assert grid_out == evolve(grid_in)
+
+
+def test_seats_with_4_or_more_neighbours_are_vacated():
+    grid_in = read_grid("#.##.##.##\n"
+                        "#######.##\n"
+                        "#.#.#..#..\n"
+                        "####.##.##\n"
+                        "#.##.##.##\n"
+                        "#.#####.##\n"
+                        "..#.#.....\n"
+                        "##########\n"
+                        "#.######.#\n"
+                        "#.#####.##")
+    grid_out = read_grid("#.LL.L#.##\n"
+                         "#LLLLLL.L#\n"
+                         "L.L.L..L..\n"
+                         "#LLL.LL.L#\n"
+                         "#.LL.LL.LL\n"
+                         "#.LLLL#.##\n"
+                         "..L.L.....\n"
+                         "#LLLLLLLL#\n"
+                         "#.LLLLLL.L\n"
+                         "#.#LLLL.##")
+    assert grid_out == evolve(grid_in)
