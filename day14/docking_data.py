4221297173c80f62d170670751f90646f3147fcc
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-15 00:33:36 +0100

    do the masking

--- day14/docking_data.py
+++ day14/docking_data.py
@@ -1,4 +1,26 @@
+import re
+
+
 def apply_bitmasks(input):
     mem = {}
-    mem[8] = 73
+    for line in input.splitlines():
+        maskline = re.match(r'^mask = ([01X]{36})', line)
+        memline = re.match(r'^mem\[(\d+)\] = (\d+)', line)
+        if maskline:
+            mask = maskline.group(1)
+            print(mask)
+        if memline:
+            address = memline.group(1)
+            number = int(memline.group(2))
+            number_bin = bin(number)[2:].zfill(36)
+            print(address)
+            print(number)
+
+            masked_number = []
+            for i, n in enumerate(mask):
+                if n == 'X':
+                    masked_number.append(number_bin[i])
+                else:
+                    masked_number.append(n)
+            mem[int(address)] = int(''.join(masked_number), 2)
     return mem
--- day14/test_docking_data.py
+++ day14/test_docking_data.py
@@ -6,3 +6,10 @@ def test_read_one_block_one_number():
              "mem[8] = 11\n")
     mem = apply_bitmasks(input)
     assert 73 == mem[8]
+
+
+def test_read_one_block_one_other_number():
+    input = ("mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X\n"
+             "mem[7] = 101\n")
+    mem = apply_bitmasks(input)
+    assert 101 == mem[7]
