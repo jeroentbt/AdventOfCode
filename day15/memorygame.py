b04409d1041e352f4f99b1c9f70f6c42982e8cb6
Jeroen Tiebout <jeroen@mailbox.org>
2020-12-15 23:10:21 +0100

    green

--- day15/memorygame.py
+++ day15/memorygame.py
@@ -1,2 +1,16 @@
-def play_turns(turns, input):
-    return input[turns -1]
+def play_turns(turns, starting_numbers):
+    numbers = []
+    last_number = 0
+    for i in range(turns):
+        print('-' * 20, i)
+        if i < len(starting_numbers):
+            speak = starting_numbers[i]
+            print(speak)
+            numbers.append((speak, [i+1]))
+        else:
+            spoken, when = numbers[i-1]
+            if len(when) == 1:
+                return 0
+        last_number = speak
+        print(last_number)
+    return last_number
