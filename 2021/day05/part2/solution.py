09760fb0d83918fdad2b0a3ea9dad9c2cc581790
Jeroen Tiebout <jeroen@mailbox.org>
2021-12-05 16:44:47 +0100

    start part2

---
+++ 2021/day05/part2/solution.py
@@ -0,0 +1,37 @@
+from collections import Counter
+
+
+def read_coordinates(input):
+    return [tuple([tuple([int(x) for x in x_comma_y.split(',')])
+                   for x_comma_y in line.split(' -> ')])
+            for line in input.splitlines()]
+
+
+def line_for_coordinates(c):
+    a, b = c
+    line = []
+    if a[1] == b[1]:
+        y = a[1]
+        for x in range(min(a[0], b[0]), max(a[0], b[0]) + 1):
+            line.append((x, y))
+    if a[0] == b[0]:
+        x = a[0]
+        for y in range(min(a[1], b[1]), max(a[1], b[1]) + 1):
+            line.append((x, y))
+    return line
+
+
+def list_overlaps(coordinates):
+    return [point for point, count in Counter(coordinates).items() if count > 1]
+
+
+def number_of_overlapping_vents(input):
+    points = []
+    for c in read_coordinates(input):
+        points += line_for_coordinates(c)
+    return(len(list_overlaps(points)))
+
+
+if __name__ == "__main__":
+    with open("../input.txt") as input:
+        print(number_of_overlapping_vents(input.read()))
---
+++ 2021/day05/part2/test_solution.py
@@ -0,0 +1,49 @@
+from solution import read_coordinates, \
+    line_for_coordinates, list_overlaps, \
+    number_of_overlapping_vents
+
+
+def test_read_coordinates_for_2_lines():
+    input = "0,0 -> 1,0\n" \
+        "3,0 -> 4,0\n"
+    expected = [((0, 0), (1, 0)),
+                ((3, 0), (4, 0))]
+    assert expected == read_coordinates(input)
+
+
+def test_set_points_of_horizontal_line():
+    input = ((0, 0), (4, 0))
+    expected = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0)]
+    assert expected == line_for_coordinates(input)
+
+
+def test_set_points_of_vertical_line():
+    input = ((0, 0), (0, 4))
+    expected = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4)]
+    assert expected == line_for_coordinates(input)
+
+
+def test_set_points_of_reversed_horizontal_line():
+    input = ((4, 0), (0, 0))
+    expected = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0)]
+    assert expected == line_for_coordinates(input)
+
+
+def test_find_single_overlap():
+    input = [(0, 0), (0, 0), (1, 0)]
+    expected = [(0, 0)]
+    assert expected == list_overlaps(input)
+
+
+def test_example():
+    input = "0,9 -> 5,9\n" \
+        "8,0 -> 0,8\n" \
+        "9,4 -> 3,4\n" \
+        "2,2 -> 2,1\n" \
+        "7,0 -> 7,4\n" \
+        "6,4 -> 2,0\n" \
+        "0,9 -> 2,9\n" \
+        "3,4 -> 1,4\n" \
+        "0,0 -> 8,8\n" \
+        "5,5 -> 8,2\n"
+    assert 12 == number_of_overlapping_vents(input)
