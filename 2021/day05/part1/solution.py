b29e6124ef6ee5c76ea7873b27e3b94ba7df5623
Jeroen Tiebout <jeroen@mailbox.org>
2021-12-05 11:51:56 +0100

    refactor

--- 2021/day05/part1/solution.py
+++ 2021/day05/part1/solution.py
@@ -2,10 +2,10 @@ def read_coordinates(input):
     input = input.splitlines()
     list_of_coordinates = []
     for line in input:
-        ventlines_text = line.split(' -> ')
-        c_start = tuple([int(x) for x in ventlines_text[0].split(',')])
-        c_end = tuple([int(x) for x in ventlines_text[1].split(',')])
-        list_of_coordinates.append(tuple([c_start, c_end]))
+        c = tuple([tuple([int(x) for x in x_comma_y.split(',')])
+             for x_comma_y in line.split(' -> ')])
+
+        list_of_coordinates.append(c)
 
     return list_of_coordinates
 
