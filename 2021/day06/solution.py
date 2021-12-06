f243517071c79cba3b59c9cc5b0d3fa15b3d5356
Jeroen Tiebout <jeroen@mailbox.org>
2021-12-06 22:57:05 +0100

    small refactor
    ...

--- 2021/day06/solution.py
+++ 2021/day06/solution.py
@@ -14,14 +14,10 @@ def input_to_dict(input):
 
 def one_day_later(younger):
     older = {}
-    older[0] = younger[1]
-    older[1] = younger[2]
-    older[2] = younger[3]
-    older[3] = younger[4]
-    older[4] = younger[5]
-    older[5] = younger[6]
-    older[6] = younger[7] + younger[0]
-    older[7] = younger[8]
+    for i in range(0, 8):
+        older[i] = younger[i+1]
+    # spawn
+    older[6] += younger[0]
     older[8] = younger[0]
 
     return older
