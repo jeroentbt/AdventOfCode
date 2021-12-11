60246598e80bce9374e25982c7e4369e7d94c907
Jeroen Tiebout <jeroen@mailbox.org>
2021-12-11 01:17:18 +0100

    find lowest point if height is one

--- 2021/day09/smoke_basin.py
+++ 2021/day09/smoke_basin.py
@@ -1,2 +1,14 @@
+class Point():
+    def __init__(self, height, x, y):
+        self.height = int(height)
+        self.x = int(x)
+        self.y = int(y)
+
+
 def low_points(heightmap):
-    return [1]
+    points = []
+    for y, horizontal in enumerate(heightmap.splitlines()):
+        for x, height in enumerate(list(horizontal)):
+            print(height, x, y)
+            points.append(Point(height, x, y))
+    return [point for point in points if point.height == 1]
--- 2021/day09/test_smoke_basin.py
+++ 2021/day09/test_smoke_basin.py
@@ -6,3 +6,10 @@ def test_find_one_lowest_point():
         "91\n" \
         "99"
     assert 1 == len(low_points(heightmap))
+
+
+def test_find_two_lowest_point():
+    heightmap = \
+        "91\n" \
+        "19"
+    assert 2 == len(low_points(heightmap))
