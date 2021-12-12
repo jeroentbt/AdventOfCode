f3c9b80df61b31bd0e634db53fcb4238dd3fb360
Jeroen Tiebout <jeroen@mailbox.org>
2021-12-12 12:44:45 +0100

    part 1 done

--- 2021/day10/syntax_scoring.py
+++ 2021/day10/syntax_scoring.py
@@ -14,3 +14,22 @@ def is_corrupted(line):
             if should_close == '<' and c != '>':
                 return c
     return False
+
+
+def part1(input):
+    lines = input.splitlines()
+    illegals = []
+    for line in lines:
+        illegals.append(is_corrupted(line))
+    print(illegals)
+    syntax_error_score = 0
+    for illegal in illegals:
+        if illegal == ")":
+            syntax_error_score += 3
+        if illegal == "]":
+            syntax_error_score += 57
+        if illegal == "}":
+            syntax_error_score += 1197
+        if illegal == ">":
+            syntax_error_score += 25137
+    return syntax_error_score
--- 2021/day10/test_syntax_scoring.py
+++ 2021/day10/test_syntax_scoring.py
@@ -1,4 +1,4 @@
-from syntax_scoring import is_corrupted
+from syntax_scoring import is_corrupted, part1
 
 
 def test_simple_chunk_is_legal():
@@ -14,10 +14,38 @@ def test_nested_chunk_is_legal():
 
 
 def test_loose_legal_examples():
-    assert True
+    assert False is is_corrupted('([])')
+    assert False is is_corrupted('{()()()}')
+    assert False is is_corrupted('<([{}])>')
+    assert False is is_corrupted('[<>({}){}[([])<>]]')
+    assert False is is_corrupted('(((((((((()))))))))).')
+
+
+def test_loose_corrupted_examples():
+    assert False is not is_corrupted('(]')
+    assert False is not is_corrupted('{()()()>')
+    assert False is not is_corrupted('(((()))}')
+    assert False is not is_corrupted('<([]){()}[{}])')
+
+
+example_input = \
+    '[({(<(())[]>[[{[]{<()<>>\n' \
+    '[(()[<>])]({[<{<<[]>>(\n' \
+    '{([(<{}[<>[]}>{[]{[(<()>\n' \
+    '(((({<>}<{<{<>}{[]{[]{}\n' \
+    '[[<[([]))<([[{}[[()]]]\n' \
+    '[{[{({}]{}}([{[{{{}}([]\n' \
+    '{<[[]]>}<{[{[{[]{()[[[]\n' \
+    '[<(<(<(<{}))><([]([]()\n' \
+    '<{([([[(<>()){}]>(<<{{\n' \
+    '<{([{{}}[<[[[<>{}]]]>[]]'
+
+
+def test_example_count_illegals():
+    assert 26397 == part1(example_input)
 
 
 def test_part1_solution():
     with open("input.txt") as input:
         input = input.read()
-    assert True
+    assert 392097 == part1(input)
