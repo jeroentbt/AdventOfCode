0c4910cf3e854df226f9b44e77ebc36d2835676b
Jeroen Tiebout <jeroen@mailbox.org>
2022-12-01 22:54:55 +0100

    2022 day1 done

--- 2022/day01/solution.py
+++ 2022/day01/solution.py
@@ -1,16 +1,26 @@
-def calories_of_most_loaded_elf(list):
-    max_calories = 0
-    for elves_bag in list.split("\n\n"):
+def calories_per_elf(list):
+    total_calories_list = []
+    for elf_bag in list.split("\n\n"):
         total = 0
-        for i in elves_bag.splitlines():
+        for i in elf_bag.splitlines():
             total += int(i)
-        if total > max_calories:
-            max_calories = total
-    return max_calories
+        total_calories_list.append(total)
+    return total_calories_list
+
+def calories_of_most_loaded_elf(list):
+    return max(calories_per_elf(list))
+
+
+def calories_of_3_most_loaded_elves(list):
+    s = sorted(calories_per_elf(list), reverse=True)
+    return s[0] + s[1] + s[2]
 
 
 
 if __name__ == "__main__":
     with open("input.txt") as elves_list:
         lines = elves_list.read()
+        print("Part 1: Most loaded elf carries:")
         print(calories_of_most_loaded_elf(lines))
+        print("Part 2: 3 most loaded elves carry:")
+        print(calories_of_3_most_loaded_elves(lines))
--- 2022/day01/test_solution.py
+++ 2022/day01/test_solution.py
@@ -14,7 +14,7 @@ def test_most_calories_of_two_elves():
     assert 2 == calories_of_most_loaded_elf(elves_list)
 
 
-def test_example():
+def test_example_part1():
     elves_list = "1000\n" \
     "2000\n" \
     "3000\n" \
@@ -30,3 +30,21 @@ def test_example():
     "\n"\
     "10000"
     assert 24000 == calories_of_most_loaded_elf(elves_list)
+
+
+def test_example_part2():
+    elves_list = "1000\n" \
+    "2000\n" \
+    "3000\n" \
+    "\n"\
+    "4000\n" \
+    "\n"\
+    "5000\n" \
+    "6000\n" \
+    "\n"\
+    "7000\n" \
+    "8000\n" \
+    "9000\n" \
+    "\n"\
+    "10000"
+    assert 45000 == calories_of_3_most_loaded_elves(elves_list)
