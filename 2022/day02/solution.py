696e197d354d3b6521c96a7a1064596e1a9e75ce
Jeroen Tiebout <jeroen@mailbox.org>
2022-12-02 21:13:20 +0100

    day2 part2 I feel dirty...

--- 2022/day02/solution.py
+++ 2022/day02/solution.py
@@ -30,6 +30,42 @@ def score_round(round):
     return score
 
 
+def score_strategized_round(round):
+    # A = Rock, 1
+    # B = Paper, 2
+    # C = Scissor, 3
+    # X = lose
+    # Y = draw
+    # Z = win
+
+    hands = {"A": {"beats": "C",
+                   "beaten_by": "B",
+                   "score": 1},
+             "B": {"beats": "A",
+                   "beaten_by": "C",
+                   "score": 2},
+             "C": {"beats": "B",
+                   "beaten_by": "A",
+                   "score": 3}}
+
+    their_play, my_play = round.split(" ")
+    my_play = my_play.strip()
+
+    if my_play == "X":  # Lose
+        score = 0
+        my_play = hands[their_play]["beats"]
+    if my_play == "Y":  # Draw
+        score = 3
+        my_play = their_play
+    if my_play == "Z":  # Win
+        score = 6
+        my_play = hands[their_play]["beaten_by"]
+
+    score += hands[my_play]["score"]
+
+    return score
+
+
 
 if __name__ == "__main__":
     with open("input.txt") as strategy_guide:
@@ -37,4 +73,8 @@ if __name__ == "__main__":
         total_points = 0
         for round in rounds:
             total_points += score_round(round)
-        print(total_points)
+        print("part 1 " + str(total_points))
+        total_points2 = 0
+        for round in rounds:
+            total_points2 += score_strategized_round(round)
+        print("part 2 " + str(total_points2))
--- 2022/day02/test_solution.py
+++ 2022/day02/test_solution.py
@@ -1,12 +1,23 @@
 from solution import *
 import pytest
 
-testdata = [
+testdata_1 = [
     ("A Y", 8),
     ("B X", 1),
     ("C Z", 6)
 ]
 
-@pytest.mark.parametrize("round,score", testdata)
+@pytest.mark.parametrize("round,score", testdata_1)
 def test_score_rounds(round, score):
     assert score == score_round(round)
+
+
+testdata_2 = [
+    ("A Y", 4),
+    ("B X", 1),
+    ("C Z", 7)
+]
+
+@pytest.mark.parametrize("round,score", testdata_2)
+def test_score_rounds_2(round, score):
+    assert score == score_strategized_round(round)
