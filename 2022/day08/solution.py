57a39b0163988c8ebcfffbed84b6d31d56d52f34
Jeroen Tiebout <jeroen@mailbox.org>
2022-12-09 01:42:52 +0100

    day08 part 2 pff

--- 2022/day08/solution.py
+++ 2022/day08/solution.py
@@ -1,3 +1,6 @@
+import math
+
+
 def visible_trees(input):
     treegrid = set_visibility(parse_input(input))
     total_visible = 0
@@ -9,7 +12,7 @@ def visible_trees(input):
                 printer += "\033[91m" + str(tree["size"]) + "\033[00m"
             else:
                 printer += str(tree["size"])
-        print(printer)
+        # print(printer)
     return total_visible
 
 
@@ -17,7 +20,8 @@ def parse_input(input):
     treegrid = []
     for line in list(input.split("\n")):
         treegrid.append([{"size": int(i),
-                          "visible": None} for i in list(line.strip())])
+                          "visible": None,
+                          "scenic score": 0} for i in list(line.strip())])
     return treegrid
 
 
@@ -55,8 +59,55 @@ def set_visibility(treegrid):
     return treegrid
 
 
+def best_trees_scenic_score(input):
+    treegrid = parse_input(input)
+    best = 0
+    for y in range(len(treegrid)):
+        for x in range(len(treegrid[0])):
+            treegrid = set_scenic_score(treegrid, x, y)
+            if treegrid[y][x]["scenic score"] > best:
+                best = treegrid[y][x]["scenic score"]
+    return best
+
+
+def set_scenic_score(treegrid, x, y):
+    rows = len(treegrid)
+    columns = len(treegrid[0])
+
+    lines_of_sight = []
+    # left to right AND right to left
+    line = []
+    for x_ in range(columns):
+        line.append((x_, y))
+    lines_of_sight.append(line[x:])
+    lines_of_sight.append(list(reversed(line[:x])))
+    # top to bottom AND bottom to top
+    line = []
+    for y_ in range(rows):
+        line.append((x, y_))
+    lines_of_sight.append(line[y:])
+    lines_of_sight.append(list(reversed(line[:y])))
+
+    this_tree_size = treegrid[y][x]["size"]
+    viewing_distances = []
+    for line in lines_of_sight:
+        line = [t for t in line if t != (x, y)]
+        viewing_distance = 0
+        for x_, y_ in line:
+            viewing_distance += 1
+            if treegrid[y_][x_]["size"] >= this_tree_size:
+                break
+
+        viewing_distances.append(viewing_distance)
+    treegrid[y][x]["scenic score"] = math.prod(viewing_distances)
+
+    return treegrid
+
+
 if __name__ == "__main__":
     with open("input.txt") as input:
         i = input.read().rstrip()
     print("part 1:")
     print("visible trees: " + str(visible_trees(i)))
+    print("part 2:")
+    print("best scenic score: " + str(best_trees_scenic_score(i)))
--- 2022/day08/test_solution.py
+++ 2022/day08/test_solution.py
@@ -28,3 +28,18 @@ def test_solution_part_1():
     with open("input.txt") as txt:
         input = txt.read().rstrip()
     assert 1679 == visible_trees(input)
+
+
+def test_example_part_2():
+    input = ("30373\n"
+             "25512\n"
+             "65332\n"
+             "33549\n"
+             "35390")
+    assert 8 == best_trees_scenic_score(input)
+
+
+def test_solution_part_2():
+    with open("input.txt") as txt:
+        input = txt.read().rstrip()
+    assert 536625 == best_trees_scenic_score(input)
