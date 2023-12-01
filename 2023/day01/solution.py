d30af13831a4b00353a37e6bfa78aa9ce5ba2e9d
Jeroen Tiebout <jeroen@mailbox.org>
2023-12-01 10:51:35 +0100

    part 1 solved

--- 2023/day01/solution.py
+++ 2023/day01/solution.py
@@ -5,10 +5,10 @@ def first_and_last_digit_of(input_string):
     digits = re.findall(r'\d', input_string)
     return digits[0], digits[-1]
 
+
 def sum_of_values(input_file):
-    sum = 0
+    output_sum = 0
     with open(input_file) as the_input:
         for line in the_input.readlines():
-            sum += int("".join(first_and_last_digit_of(line)))
-    return sum
-
+            output_sum += int("".join(first_and_last_digit_of(line)))
+    return output_sum
