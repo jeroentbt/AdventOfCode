7d9c1c60acdceb8c09a75149452a0e332fa45fa2
Jeroen Tiebout <jeroen@mailbox.org>
2023-12-03 00:15:02 +0100

    games parse

--- 2023/day02/solution.py
+++ 2023/day02/solution.py
@@ -1,9 +1,40 @@
 from collections import namedtuple
 from typing import Dict, List
+import regex as re
+from icecream import ic
 
 Rgb = namedtuple('Rgb', ['R', 'G', 'B'])
 
 
 def parse_game(game: str) -> Dict[int, List[Rgb]]:
-    parsed_game = {1: [Rgb(R=4, G=0, B=3), Rgb(R=1, G=2, B=6), Rgb(R=0, G=2, B=0)]}
+    pattern = r'^Game (?P<game_nr>\d+):(?P<sets>.*)'
+    match = re.search(pattern, game)
+
+    if match:
+        game_nr = int(match.group("game_nr"))
+        sets = [read_set(s) for s in match.group("sets").split(';')]
+
+    else:
+        return {}
+
+    parsed_game = {game_nr: sets}
     return parsed_game
+
+
+def read_set(setstring: str) -> Rgb:
+    ic(setstring)
+    red = 0
+    green = 0
+    blue = 0
+
+    for count in setstring.split(','):
+        pattern = r'(?P<nr>\d+) (?P<color>red|green|blue)'
+        match = re.search(pattern, count)
+
+        if match:
+            red = int(match.group("nr")) if match.group("color") == "red" else red
+            green = int(match.group("nr")) if match.group("color") == "green" else green
+            blue = int(match.group("nr")) if match.group("color") == "blue" else blue
+
+    parsed_set = Rgb(R=red, G=green, B=blue)
+    return parsed_set
--- 2023/day02/test_solution.py
+++ 2023/day02/test_solution.py
@@ -8,7 +8,10 @@ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
 
 games_parsed = [("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-                 {1: [Rgb(R=4, G=0, B=3), Rgb(R=1, G=2, B=6), Rgb(R=0, G=2, B=0)]})]
+                 {1: [Rgb(R=4, G=0, B=3), Rgb(R=1, G=2, B=6), Rgb(R=0, G=2, B=0)]}),
+                ("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+                 {2: [Rgb(R=0, G=2, B=1), Rgb(R=1, G=3, B=4), Rgb(R=0, G=1, B=1)]})
+                ]
 
 
 @pytest.mark.parametrize("game,expected", games_parsed)
